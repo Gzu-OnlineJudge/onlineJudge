51af83ff3f934c08e457b9f62b060ece0c51aa6e
Gzuwkj <1838679406@qq.com>
2020-04-07 21:43:31 +0800

    2020/4/7 21：43

---
+++ frontend/src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+        <div style="height: 1000px">
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        created() {
+            console.log(this.$route)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
